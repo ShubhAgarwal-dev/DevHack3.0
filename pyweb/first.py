118bd25eab1942c00a90f30468603aee2c5480f2
Shubh Agarwal <sazorinos1111@gmail.com>
2022-03-05 01:50:21 +0530

    Social Hackers

--- pyweb/first.py
+++ pyweb/first.py
@@ -1,7 +1,40 @@
 import sqlite3
+from os.path import isfile
 
 
 class log_in():
 
-    def __init__(self, db_path: str):
-        pass
+    def __init__(self, db_path: str) -> None:
+        self.db_path = db_path
+        if not isfile(path=db_path):
+            self._make_database(self.db_path)
+
+    @staticmethod
+    def _make_database(db_path):
+        connection = sqlite3.connect(db_path)
+        connection.execute("""
+            CREATE TABLE "users"(
+	            "name"	TEXT,
+	            "dob"	INTEGER,
+	            "email"	TEXT,
+	            "img"	INTEGER,
+	            "password"	TEXT,
+	            PRIMARY KEY("name")
+            );
+        
+        """)
+        connection.commit()
+        connection.close()
+
+    def add_user(self, user_name: str, dob: int, email: str, img_block_index: int, password: str):
+        connection = sqlite3.connect(self.db_path)
+        connection.execute(
+            """
+            INSERT INTO "users"("name","dob","email","img","password") VALUES(?,?,?,?,?)
+            """, [str(user_name), int(dob), str(email), int(img_block_index), str(password)])
+        connection.commit()
+        connection.close()
+
+    def check_user(self, user_name, password):
+        connection = sqlite3.connect()
+        connection.execute()
