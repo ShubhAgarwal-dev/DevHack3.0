35534a229720ded4bd936b3839bdfd208c4472d7
Shubh Agarwal <sazorinos1111@gmail.com>
2022-03-05 23:47:05 +0530

    just dome more changes

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
+from pyweb.first import user_db
+
+user_database = user_db(r'database\user.db')
 
 app = Flask(__name__)
+
+user_name = 'ShubhAgarwal'
 
 
 @app.route('/', methods=["GET", "POST"])
 def log_in():
     if request.method == "POST":
+        global user_name
         user_name = request.form.get("uname")
         # password = request.form.get("pword")
-        return redirect(url_for('home'))
+        if user_database.check_user_img2(user_name) > 0:
+            return redirect(url_for('home'))
     return render_template('index.html')
 
 
@@ -18,7 +25,7 @@ def home():
     return render_template('homepage.html', name=name)
 
 
-@app.route('/profile/<username>')
+@app.route('/profile')
 def profile():
     return render_template('profilepage.html')
 
